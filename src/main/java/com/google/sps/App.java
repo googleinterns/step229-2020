d7b9297b577f999fa0fd964b1130b90708fa716b
Andreea Nica <andre.nic10@yahoo.com>
2020-08-12 09:23:21 +0000

    Set up the project

---
+++ src/main/java/com/google/sps/App.java
@@ -0,0 +1,16 @@
+package com.google.sps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+		App app = new App();
+        System.out.println(app.getMsg());
+    }
+
+	public String getMsg() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/google/sps/AppTest.java
@@ -0,0 +1,17 @@
+package com.google.sps;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void getMsg() {
+		App app = new App();
+        assertEquals("Hello World!", app.getMsg());
+    }
+}
